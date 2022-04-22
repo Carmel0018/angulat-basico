30a6359f22a9359ae41477825d80cf9c0eb105b6
Carmelo <cbuisacliarte@deloitte.es>
2022-04-22 12:56:23 +0200

    primer commit

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+    declarations:[
+        HeroeComponent,
+        ListadoComponent
+    ],
+
+    exports:[
+        ListadoComponent
+    ],
+    imports:[
+       CommonModule
+
+    ]
+
+})
+
+
+export class HeroesModule{
+
+}
